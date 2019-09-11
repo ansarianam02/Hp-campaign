abb845069a9d1dd6892f98ba59eb17cf128f35c1
Anam Ansari <ansarianam02@users.noreply.github.com>
2019-09-11 17:01:51 +0530

    Update hpScript.js
    script for sticky header added

--- js/hpScript.js
+++ js/hpScript.js
@@ -3,3 +3,17 @@ $(".next-arrow").click(function() {
         scrollTop: $("#elementtoScrollToID").offset().top
     }, 2000);
 });
+
+$(function() {
+    //caches a jQuery object containing the header element
+    var header = $(".navbar");
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 50) {
+            header.removeClass('clearHeader').addClass("bg-purple fixed-top");
+        } else {
+            header.removeClass("bg-purple fixed-top").addClass('clearHeader');
+        }
+    });
+});
